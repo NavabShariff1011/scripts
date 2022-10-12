abb48b796f89b6cde77c7cb3193cbf2f0091b83d
root <root@ip-172-31-38-254.ap-south-1.compute.internal>
2022-10-12 14:25:37 +0000

    source

--- node-nvm.sh
+++ node-nvm.sh
@@ -4,10 +4,9 @@ curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o inst
 bash install_nvm.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
+source ~/.bashrc
 nvm -v
-
+read -p "enter the node version": nodeV
 nvm install ${nodeV}
 
 nvm ls
